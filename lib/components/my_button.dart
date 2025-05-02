dd126428a0dad33be1ff55ede6248a015cc72f95
Alemack71 <alem4ck@gmail.com>
2025-05-02 14:51:56 -0300

    Atualizou

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-
   final Function()? onTap;
   final String text;
 
-  const MyButton({
-    super.key, 
-    required this.onTap,
-    required this.text,
-    });
-
+  const MyButton({super.key, required this.onTap, required this.text});
+  //Atualizou
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
